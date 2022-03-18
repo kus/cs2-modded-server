5f3f6ca79794917f4eceea9f982a64004e70d5d7
Blake Kus <blakekus@gmail.com>
2022-03-18 13:49:38 +1100

    - UPDATED: Start script to move map higher as it's not loading dust2 as default map

--- start.sh
+++ start.sh
@@ -1,12 +1,56 @@
 #!/usr/bin/env bash
 
+# Install
+# As root (sudo su)
+# cd / && curl --silent --output "gcp.sh" "https://raw.githubusercontent.com/kus/csgo-modded-server/master/gcp.sh" && chmod +x gcp.sh && bash gcp.sh
+
+METADATA_URL="${METADATA_URL:-http://metadata.google.internal/computeMetadata/v1/instance/attributes}"
+
+get_metadata () {
+    if [ -z "$1" ]
+    then
+        local result=""
+    else
+        local result=$(curl -s "$METADATA_URL/$1?alt=text" -H "Metadata-Flavor: Google")
+		if [[ $result == *"<!DOCTYPE html>"* ]]; then
+			result=""
+		fi
+    fi
+
+    echo $result
+}
+
+# Get meta data from GCP and set environment variables
+META_RCON_PASSWORD=$(get_metadata RCON_PASSWORD)
+META_API_KEY=$(get_metadata API_KEY)
+META_MOD_URL=$(get_metadata MOD_URL)
+META_PORT=$(get_metadata PORT)
+META_TICKRATE=$(get_metadata TICKRATE)
+META_MAXPLAYERS=$(get_metadata MAXPLAYERS)
+export LAN="${LAN:-$(get_metadata LAN)}"
+export RCON_PASSWORD="${META_RCON_PASSWORD:-changeme}"
+export API_KEY="${META_API_KEY:-changeme}"
+export STEAM_ACCOUNT="${STEAM_ACCOUNT:-$(get_metadata STEAM_ACCOUNT)}"
+export FAST_DL_URL="${FAST_DL_URL:-$(get_metadata FAST_DL_URL)}"
+export MOD_URL="${META_MOD_URL:-https://github.com/kus/csgo-modded-server/archive/master.zip}"
+export SERVER_PASSWORD="${SERVER_PASSWORD:-$(get_metadata SERVER_PASSWORD)}"
+export PORT="${META_PORT:-27015}"
+export TICKRATE="${META_TICKRATE:-128}"
+export MAXPLAYERS="${META_MAXPLAYERS:-32}"
+export DUCK_DOMAIN="${DUCK_DOMAIN:-$(get_metadata DUCK_DOMAIN)}"
+export DUCK_TOKEN="${DUCK_TOKEN:-$(get_metadata DUCK_TOKEN)}"
+
+cd /
+
+# Update DuckDNS with our current IP
+if [ ! -z "$DUCK_TOKEN" ]; then
+    echo url="http://www.duckdns.org/update?domains=$DUCK_DOMAIN&token=$DUCK_TOKEN&ip=$(dig +short myip.opendns.com @resolver1.opendns.com)" | curl -k -o /duck.log -K -
+fi
+
 # Variables
 user="steam"
 IP="0.0.0.0"
 PUBLIC_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
-
-# Download latest stop script
-curl --silent --output "stop.sh" "https://raw.githubusercontent.com/kus/csgo-modded-server/master/stop.sh" && chmod +x stop.sh
 
 # Check distrib
 if ! command -v apt-get &> /dev/null; then
@@ -80,12 +124,6 @@ echo "Downloading any updates for CS:GO..."
 cd /home/${user}/csgo/csgo/warmod/ && python3 -m http.server 80 </dev/null &>/dev/null &
 
 cd /home/${user}
-
-echo "Downloading mod files..."
-wget --quiet https://github.com/kus/csgo-modded-server/archive/master.zip
-unzip -o -qq master.zip
-cp -rlf csgo-modded-server-master/csgo/ /home/${user}/csgo/
-rm -r csgo-modded-server-master master.zip
 
 echo "Dynamically writing /home/$user/csgo/csgo/cfg/env.cfg"
 echo "rcon_password						\"$RCON_PASSWORD\"" > /home/${user}/csgo/csgo/cfg/env.cfg
