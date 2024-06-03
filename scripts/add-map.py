fd3429ad8b99a56abdb802cacff566525b54e486
vichoko <vicenteoyanedel@gmail.com>
2024-06-03 01:17:07 -0400

    fix: avoid duplicating file_ids

--- scripts/add-map.py
+++ scripts/add-map.py
@@ -21,20 +21,14 @@ import pathlib
 import sys
 import argparse
 
-gamemodes_server_path = pathlib.Path("game/csgo/gamemodes_server.txt")
-subscribed_file_ids_path = pathlib.Path("game/csgo/subscribed_file_ids.txt")
-
-custom_gamemodes_server_path = pathlib.Path("custom_files/gamemodes_server.txt")
-custom_subscribed_file_ids_path = pathlib.Path("custom_files/subscribed_file_ids.txt")
-
 
 def add_map_to_group(
-    gamemodes_server_path, group_name: str, map_name: str, workshop_id: str = None
+    file_path, group_name: str, map_name: str, workshop_id: str = None
 ):
     """Add a map to the given game mode map group.
 
     Args:
-        gamemodes_server_path (pathlib.Path): Path to the gamemodes_server.txt file.
+        file_path (pathlib.Path): Path to the gamemodes_server.txt file.
         group_name (str): The name of the game mode group. E.g. mg_aim.
         map_name (str): The name of the map. E.g. aim_ak-colt_CS2.
         workshop_id (str, optional): The workshop ID of the map. Defaults to None. E.g.
@@ -42,52 +36,49 @@ def add_map_to_group(
     """
     if workshop_id:
         map_name = f"workshop/{workshop_id}/{map_name}"
-
-    with open(gamemodes_server_path, "r") as file:
+    with open(file_path, "r") as file:
         lines = file.readlines()
 
-    # Flags to track the position in the file
-    in_mapgroups = False
-    in_group = False
-    maps_section = False
-
+    in_mapgroups, in_group, maps_section = False, False, False
     for i, line in enumerate(lines):
         stripped_line = line.strip()
-
         if stripped_line.startswith('"mapgroups"'):
             in_mapgroups = True
-
         if in_mapgroups and stripped_line.startswith(f'"{group_name}"'):
             in_group = True
-
         if in_group and stripped_line.startswith('"maps"'):
             maps_section = True
-
         if maps_section and stripped_line == "}":
-            # Insert the new map entry just before the closing brace of the maps section
-            lines.insert(i, f'                "{map_name}"    ""\n')
+            lines.insert(i, f'				"{map_name}"		""\n')
             break
 
-    # Write the modified lines back to the file
-    with open(gamemodes_server_path, "w") as file:
+    with open(file_path, "w") as file:
         file.writelines(lines)
     print(f"Added map {map_name} to group {group_name}.")
-
     if workshop_id:
         add_workshop_id(subscribed_file_ids_path, workshop_id)
 
 
-def add_workshop_id(subscribed_file_ids_path, workshop_id: str):
+def add_workshop_id(file_path, workshop_id: str):
     """Add a workshop ID to the subscribed_file_ids.txt.
 
     Args:
-        subscribed_file_ids_path (pathlib.Path): Path to the subscribed_file_ids.txt
+        file_path (pathlib.Path): Path to the subscribed_file_ids.txt
             file.
         workshop_id (str): The workshop ID to add. E.g. 3078701726
     """
-    with open(subscribed_file_ids_path, "a") as file:
-        file.write(f"{workshop_id}\n")
-    print(f"Added workshop ID {workshop_id} to subscribed_file_ids.txt.")
+    if file_path.exists():
+        with open(file_path, "r") as file:
+            existing_ids = file.read().splitlines()
+    else:
+        existing_ids = []
+
+    if workshop_id not in existing_ids:
+        with open(file_path, "a") as file:
+            file.write(f"{workshop_id}\n")
+        print(f"Added workshop ID {workshop_id} to {file_path}.")
+    else:
+        print(f"Workshop ID {workshop_id} already exists in {file_path}.")
 
 
 if __name__ == "__main__":
@@ -112,21 +103,25 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
+    gamemodes_server_path = pathlib.Path(
+        "custom_files/gamemodes_server.txt"
+        if args.custom
+        else "game/csgo/gamemodes_server.txt"
+    )
+    subscribed_file_ids_path = pathlib.Path(
+        "custom_files/subscribed_file_ids.txt"
+        if args.custom
+        else "game/csgo/subscribed_file_ids.txt"
+    )
 
-    if args.custom:
-        gamemodes_server_path = custom_gamemodes_server_path
-        subscribed_file_ids_path = custom_subscribed_file_ids_path
-
-        # Check if custom files exist
-        if not gamemodes_server_path.exists() or not subscribed_file_ids_path.exists():
-            print(
-                "Error: Custom files do not exist. Please create the custom files "
-                "before using this tool."
-            )
-            sys.exit(1)
-    else:
-        gamemodes_server_path = gamemodes_server_path
-        subscribed_file_ids_path = subscribed_file_ids_path
+    if args.custom and (
+        not gamemodes_server_path.exists() or not subscribed_file_ids_path.exists()
+    ):
+        print(
+            "Error: Custom files do not exist. Please create the custom files before "
+            "using this tool."
+        )
+        sys.exit(1)
 
     add_map_to_group(
         gamemodes_server_path, args.group_name, args.map_name, args.workshop_id
